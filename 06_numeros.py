9e4ae47bb8cf82719002e8ab8d637302bfb79078
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-12 21:02:23 -0300

    feat: working with numbers

---
+++ 06_numeros.py
@@ -0,0 +1,24 @@
+"""
+Numeros
+"""
+
+NUMERO = 2  # INT
+DECIMAL = 1.2  # FLOAT
+IMAGINARIO = 2 + 2J  # 2 + +I
+
+NUMERO += 2  # NUMERO = NUMERO + 2
+print("NUMERO = NUMERO + 2 = ", NUMERO)
+NUMERO -= 2  # NUMERO = NUMERO - 2
+print("NUMERO = NUMERO - 2 = ", NUMERO)
+NUMERO *= 2  # NUMERO = NUMERO * 2
+print("NUMERO = NUMERO * 2 = ", NUMERO)
+NUMERO /= 2  # NUMERO = NUMERO / 2
+print("NUMERO = NUMERO / 2 = ", NUMERO)
+
+print(1+3)
+print(1-3)
+print(1*3)
+print(1/3)
+print(1//3)
+print(8 % 3)
+print(2**3)
