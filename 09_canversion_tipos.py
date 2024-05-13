02188b350e8f2dca479a4deb04544ad01f03e638
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-12 22:36:01 -0300

    feat: type conversion

---
+++ 09_canversion_tipos.py
@@ -0,0 +1,10 @@
+"""
+Conversión de tipos
+"""
+# Metodos para convertir tipos
+# int() - a Integer
+# str() - a String
+# float() - a float
+# bool() - a bolean
+#   False = "", 0, none
+#   truthy = todos los demas valores
