e1a8c46015dd49bfdb43e180067ecab1aa98aedb
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-13 12:39:29 -0300

    feat: conditionals

---
+++ 12_if.py
@@ -0,0 +1,12 @@
+""" If """
+
+EDAD = 15
+
+if EDAD > 54:
+    print("Puedes ver la pelicula con desciento")
+elif EDAD > 17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes ver la pelicula")
+
+print("listo")
