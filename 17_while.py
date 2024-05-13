ee62de68b38a07605702b85ed6e871242e17e848
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-13 14:38:56 -0300

    feat: while

---
+++ 17_while.py
@@ -0,0 +1,16 @@
+"""
+While
+"""
+
+NUMERO = 1
+
+while NUMERO < 100:
+    print(NUMERO)
+    NUMERO *= 2
+
+
+COMANDO = ""
+
+while COMANDO != "salir":
+    COMANDO = input("$")
+    print(COMANDO)
