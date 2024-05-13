20917e5341e553fdb4670dab140080e89cb6835e
Ariel Barrios <ariel29111978@hotmail.com>
2024-05-12 22:00:32 -0300

    feat: methods with numbers

---
+++ 07_metodos_number.py
@@ -0,0 +1,20 @@
+"""
+Metodos 
+"""
+
+import math
+
+# Metodos core
+print(round(1.3))  # return 1
+print(round(1.7))  # return 2
+print(round(1.5))  # return 2
+print(abs(-77))  # return 77
+print(abs(55))  # return 55
+
+# Metodos match
+
+print(math.ceil(1.1))  # return 2
+print(math.floor(1.9999))  # return 1
+print(math.isnan(23))  # return false
+print(math.pow(2, 3))  # return 8.0
+print(math.sqrt(16))  # return 4.0
